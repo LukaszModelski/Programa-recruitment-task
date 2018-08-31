38d094e96e4f770a22094cc8b2ec14eb36f623dc
Czapa <lukaszmodelski90@gmail.com>
2018-08-31 09:36:03 +0200

    Added initial age to some herose

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,12 +3,12 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Mr. Nice' },
+      { id: 11, name: 'Mr. Nice', age: 90 },
       { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
+      { id: 13, name: 'Bombasto', age: 150 },
       { id: 14, name: 'Celeritas' },
       { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
+      { id: 16, name: 'RubberMan', age: 323 },
       { id: 17, name: 'Dynama' },
       { id: 18, name: 'Dr IQ' },
       { id: 19, name: 'Magma' },
